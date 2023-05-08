e1edfc08c7845ac03d5a9e307fee78b243eee3fb
Noah Attwood <noah.attwood@dal.ca>
2023-05-07 23:10:29 -0300

    initial project creation

---
+++ include/expression_tree/expression_tree.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+
+namespace attwoodn::expression_tree {
+    static inline void say_hello() {
+        std::cout << "hello world!" << std::endl;
+    }
+}
---
+++ tests/test.cpp
@@ -0,0 +1,7 @@
+#include <expression_tree/expression_tree.hpp>
+
+int main(int argc, char** argv) {
+    attwoodn::expression_tree::say_hello();
+
+    return EXIT_SUCCESS;
+}
