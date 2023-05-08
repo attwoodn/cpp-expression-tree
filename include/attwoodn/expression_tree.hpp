787d12925916a14e16472095702713fd8569f957
Noah Attwood <noah.attwood@dal.ca>
2023-05-07 23:35:55 -0300

    adds operator implementations for less than, greater than, equals, and not equals

--- include/attwoodn/expression_tree.hpp
+++ include/attwoodn/expression_tree.hpp
@@ -3,6 +3,38 @@
 #include <iostream>
 
 namespace attwoodn::expression_tree {
+
+    namespace op {
+        
+        template<typename A, typename B>
+        class less_than {
+            bool operator()(A a, B b) {
+                return a < b;
+            }
+        };
+
+        template<typename A, typename B>
+        class greater_than {
+            bool operator()(A a, B b) {
+                return a > b;
+            }
+        };
+
+        template<typename A, typename B>
+        class equals {
+            bool operator()(A a, B b) {
+                return a == b;
+            }
+        };
+
+        template<typename A, typename B>
+        class not_equals {
+            bool operator()(A a, B b) {
+                return a != b;
+            }
+        };
+    }
+
     static inline void say_hello() {
         std::cout << "hello world!" << std::endl;
     }
