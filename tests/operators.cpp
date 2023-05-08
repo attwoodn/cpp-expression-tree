791d6557373ac1463af11814e93bcc6bd2e26a33
Noah Attwood <noah.attwood@dal.ca>
2023-05-07 23:43:32 -0300

    adds new operators test

---
+++ tests/operators.cpp
@@ -0,0 +1,12 @@
+#include <attwoodn/expression_tree.hpp>
+#include <cassert>
+
+using namespace attwoodn::expression_tree;
+
+int main(int argc, char** argv) {
+    std::string hello = "hello world";
+
+    //assert(op::equals(hello, hello));
+
+    return EXIT_SUCCESS;
+}
